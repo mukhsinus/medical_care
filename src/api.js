900769ffd1fc1e0252320d6e67c8c3600487a029
mukhsinus <kamilovmuhsin5@gmail.com>
2026-02-02 11:26:40 +0500

    api_base

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:8090'; // поменяй если нужно
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE,

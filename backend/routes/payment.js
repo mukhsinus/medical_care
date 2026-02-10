55647ef55d438905bc8a9a6b265d606cc792cdab
mukhsinus <kamilovmuhsin5@gmail.com>
2026-02-10 18:55:20 +0500

    logs click

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -18,7 +18,14 @@ router.post('/create', authMiddleware, createOrderAndInitPayment);
 router.post('/payme/callback', paymeCallback);
 
 // Click will call this URL (configure in Click merchant cabinet later)
-router.post('/click/callback', clickCallback);
+router.post('/click/callback', (req, res, next) => {
+  console.log('=== CLICK CALLBACK ROUTE HIT ===');
+  console.log('Time:', new Date().toISOString());
+  console.log('Content-Type:', req.headers['content-type']);
+  console.log('Headers:', JSON.stringify(req.headers, null, 2));
+  console.log('Body:', JSON.stringify(req.body, null, 2));
+  next();
+}, clickCallback);
 
 // Uzum Bank will call this URL (configure in Uzum merchant cabinet later)
 router.post('/uzum/callback', uzumCallback);
